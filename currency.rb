67185e8566aefee56baf4a8a1492a682f29d3f55
damianhouse <damianhouse@gmail.com>
2016-02-05 12:08:41 -0500

    Add subject method in currency class

--- currency.rb
+++ currency.rb
@@ -2,10 +2,9 @@ require 'byebug'
 class Currency
   def initialize(amount)
     @amount = amount
-  end
-
-  def +(other)
-    puts @amount.to_f + other.number.to_f
+    @hash =  {"$" => :USD,
+              "€"  => :EUR,
+              "¥"  => :JPY}
   end
 
   def number
@@ -16,28 +15,30 @@ class Currency
     @code
   end
 
+  def hash
+    @hash
+  end
+
+  def +(other)
+    puts @amount.to_f + other.number.to_f
+  end
+
+  def -(other)
+    puts @amount.to_f - other.number.to_f
+  end
+
   def type
-    if @amount.include?("$")
-      @amount = @amount[1..-1].to_f
-      @code = :USD
-    elsif @amount.include?("€")
-      @amount = @amount[1..-1].to_f
-      @code = :EUR
-    elsif @amount.include?("¥")
-      @amount = @amount[1..-1].to_f
-      @code = :JPY
-    elsif (@amount.number.to_f.to_s == 0.0) || (@amount.number.to_i.to_s == 0)
-      puts "UndefinedCurrencyInClass"
-    else
-      @amount.to_f
+    if @hash.has_key?(@amount.chars.first)
+      puts "WE GOT THIS SHIT B!"
     end
   end
+
 end
 
 
-myAmount = Currency.new("1.4")
+myAmount = Currency.new("4.0")
 myAmount.type
-otherAmount = Currency.new("$2.0")
+otherAmount = Currency.new("3.0")
 otherAmount.type
 
-puts myAmount + otherAmount
+puts myAmount - otherAmount
