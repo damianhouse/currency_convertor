9148c964c5dd3c01ecae6ae453157727b5aa461e
damianhouse <damianhouse@gmail.com>
2016-02-05 14:03:56 -0500

    Add instances for all math so cross currencies don't add, subtract, multiply, and equals

--- currency.rb
+++ currency.rb
@@ -1,48 +1,60 @@
 require 'byebug'
 class Currency
+  attr_accessor :number, :code, :hash
   def initialize(amount)
     @amount = amount
     @hash =  {"$" => :USD,
               "€"  => :EUR,
               "¥"  => :JPY}
-  end
+    @code = ""
 
-  def number
-    @amount
-  end
-
-  def code
-    @code
-  end
-
-  def hash
-    @hash
-  end
-
-  def +(other)
-    puts @amount.to_f + other.number.to_f
-  end
-
-  def -(other)
-    puts @amount.to_f - other.number.to_f
-  end
-
-  def *(other)
-    puts @amount.to_f * other.number.to_f
   end
 
   def type
     if @hash.has_key?(@amount.chars.first)
-      puts "WE GOT THIS SHIT B!"
+      @code = @hash[@amount.chars.first]
+      puts @code
+    end
+  end
+
+  def +(other)
+    if @code == other.code
+      puts @amount.to_f + other.number.to_f
+    else
+      puts "The currencies are different"
+    end
+  end
+
+  def -(other)
+    if @code == other.code
+      puts @amount.to_f - other.number.to_f
+    else
+      puts "The currencies are different"
+    end
+  end
+
+  def *(other)
+    if @code == other.code
+      puts @amount.to_f * other.number.to_f
+    else
+      puts "The currencies are different"
+    end
+  end
+
+  def ==(other)
+    if @code == other.code
+      puts @amount.to_f == other.number.to_f
+    else
+      puts "The currencies are different"
     end
   end
 
 end
 
 
-myAmount = Currency.new("4.0")
+myAmount = Currency.new("$4.0")
 myAmount.type
-otherAmount = Currency.new("3.0")
+otherAmount = Currency.new("$4.0")
 otherAmount.type
 
-puts myAmount * otherAmount
+puts myAmount == otherAmount
